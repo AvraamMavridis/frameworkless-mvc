b3af0a6189cfe50580e8adf8ebd23f32d3886d39
AvraamMavridis <avraammauridis@gmail.com>
2016-05-24 19:28:00 +0200

    Kinda close

--- src/views/DonutChart.js
+++ src/views/DonutChart.js
@@ -47,10 +47,41 @@ export default class DonutChart extends View
             .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
 
         const path = svg.selectAll("path")
-            .data(pie(dataset.apples))
-            .enter().append("path")
-            .attr("fill", (d, i) => this.colors[i])
-            .attr("d", arc);
+               .data(pie(dataset.apples))
+               .enter().append("path")
+               .attr("fill", (d, i) => this.colors[i])
+               .attr("d", arc);
+
+               var w = (this.radius * 2) - (this.donutWidth*2) - 10 ;
+               var h = 25;
+               var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
+           		// X scale will fit all values from data[] within pixels 0-w
+           		var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+           		// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+           		var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
+
+            var lineFun = d3.svg.line()
+              .x(function(d,i) {
+                return x(i);
+              })
+              .y(function(d) {
+                return y(d);
+              })
+              .interpolate("linear");
+
+
+
+            var viz = svg.append("path")
+              .attr({
+                d : lineFun(data),
+                "stroke" : "purple",
+                "stroke-width" : 2,
+                "fill" : "none"
+              })
+              .attr("transform", "translate(-60,0)");
+
+
+
 
     }
 
